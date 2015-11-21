1828e00676cb922bc9371748a9a7e958e8b36610
Hellux <hellux07@gmail.com>
2015-11-21 13:18:04 +0100

    removed absolute path

--- renderer_c/renderer.c
+++ renderer_c/renderer.c
@@ -5,7 +5,7 @@ int main(int argc, char* args) {
     double rot[] = {0, 0, 0};
     int col[] = {0, 0, 0};
 
-    PRJ_add_object("/home/noah/src/py/3d-projection/data/911.obj",
+    PRJ_add_object("../data/911.obj",
                    pos, rot, col, 0, 0.5);
     return EXIT_SUCCESS;
 }
