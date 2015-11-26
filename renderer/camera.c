cbb15c69a6d9ae98770c70deece3078d39ce4670
Hellux <hellux07@gmail.com>
2015-11-26 22:06:22 +0100

    made comment more clear

--- renderer/camera.c
+++ renderer/camera.c
@@ -27,7 +27,7 @@ struct Camera* camera_create(double pos[], double rot[], int res[],
 
 static void calc_array_size(struct Camera* C) {
     /*     width
-     * I-----------I   [m]
+     * |-----------|   [m]
      *  ___________ _
      *  \  |_|    / |
      *   \   |   /  |
@@ -58,28 +58,28 @@ static void calc_array_size(struct Camera* C) {
 static void calc_rotation_matrix(struct Camera* C) {
     double rot_x[3][3], rot_y[3][3], rot_z[3][3]; 
 
-    /* rotation matrix for x axis (roll):
+    /* rotation matrix for x axis (yaw):
      *         | 1   0       0       |
      * Rx(a) = | 0   cos(a)  -sin(a) |
      *         | 0   sin(a)  cos(a)  | */
     rot_x[0][0] = 1; rot_x[0][1] = 0;              rot_x[0][2] = 0;
-    rot_x[1][0] = 0; rot_x[1][1] = cos(C->rot[0]); rot_x[1][2] = -sin(C->rot[0]);
-    rot_x[2][0] = 0; rot_x[2][1] = sin(C->rot[0]); rot_x[2][2] = cos(C->rot[0]);
+    rot_x[1][0] = 0; rot_x[1][1] = cos(C->rot[0]); rot_x[1][2] = sin(C->rot[0]);
+    rot_x[2][0] = 0; rot_x[2][1] = -sin(C->rot[0]); rot_x[2][2] = cos(C->rot[0]);
     
     /* rotation matrix for y axis (pitch)
      *         | cos(b)  0   sin(b) |
      * Ry(b) = | o       1   0      |
      *         | -sin(b) 0   cos(b) | */
-    rot_y[0][0] = cos(C->rot[1]);  rot_y[0][1] = 0; rot_y[0][2] = sin(C->rot[1]);
+    rot_y[0][0] = cos(C->rot[1]);  rot_y[0][1] = 0; rot_y[0][2] = -sin(C->rot[1]);
     rot_y[1][0] = 0;               rot_y[1][1] = 1; rot_y[1][2] = 0;
-    rot_y[2][0] = -sin(C->rot[1]); rot_y[2][1] = 0; rot_y[2][2] = cos(C->rot[1]);
+    rot_y[2][0] = sin(C->rot[1]); rot_y[2][1] = 0; rot_y[2][2] = cos(C->rot[1]);
 
-    /* rotation matrix for z axis (yaw)
+    /* rotation matrix for z axis (roll)
      *         | cos(c) -sin(c) 0 |
      * Rz(c) = | sin(c) cos(c)  0 |
      *         | 0      0       1 | */
-    rot_z[0][0] = cos(C->rot[2]); rot_z[0][1] = -sin(C->rot[2]); rot_z[0][2] = 0;
-    rot_z[1][0] = sin(C->rot[2]); rot_z[1][1] = cos(C->rot[2]);  rot_z[1][2] = 0;
+    rot_z[0][0] = cos(C->rot[2]); rot_z[0][1] = sin(C->rot[2]); rot_z[0][2] = 0;
+    rot_z[1][0] = -sin(C->rot[2]); rot_z[1][1] = cos(C->rot[2]);  rot_z[1][2] = 0;
     rot_z[2][0] = 0;              rot_z[2][1] = 0;               rot_z[2][2] = 1;
     
     /* combined rotation matrix
@@ -95,15 +95,19 @@ static void calc_rotation_matrix(struct Camera* C) {
 
 void camera_calc_direction(struct Camera C, int row, int col, double V[]) {
     /*
-     *    |---- width ----|         focal length     [m]
-     * row        ^ y              |--------------|
-     *  :  _______|_______                             _
-     *  4 |_|_|_|_|_|_|_|_|        ^ y            |    |
-     *  3 |_|_|_|_|_|_|_|_|   x    |   z          |    |
-     *  2 |_|_|_|_|_|_|_|_|--->    C -->          |  height
-     *  1 |_|_|_|_|_|_|_|_|                       |    |
-     *  0 |_|_|_|_|_|_|_|_|                       |    |
-     *     0 1 2 3 4 5 6 7 .. col                      -
+     *    focal length         |---- width ----|
+     *  |---------------|  c ^         ^ y        [m]  
+     *                       |  _______|_______         _
+     *  ^ y             |    H |_|_|_|_|_|_|_|_|        |
+     *  |   z           |    : |_|_|_|_|_|_|_|_|   x    |
+     *  C -->  -  -  -  |    2 |_|_|_|_C_|_|_|_|--->  height
+     *                  |    1 |_|_|_|_|_|_|_|_|        |
+     *                  |    0 |_|_|_|_|_|_|_|_|        |
+     *                          0 1 2 3 4 ... W -->     -
+     *                                            r
+     * C: camera
+     * W, H: resolution width, height
+     * r, c: row, column
      *
      * First, create a vertex for a point in an unrotated array
      * with origo located inside the camera. The vertex will be
@@ -118,7 +122,7 @@ void camera_calc_direction(struct Camera C, int row, int col, double V[]) {
      * with the camera's rotation matrix:
      *
      * |x'|     |x|
-     * |y'| = R |y|
+     * |y'| = R |y| = R * V^T
      * |z'|     |z|
      *
      * V = (x', y', z') */
