6ad34d578eb4ca085c4efe08bfec3e6f2f690caa
Hellux <hellux07@gmail.com>
2016-03-25 18:03:52 +0100

    added vector for light, reorganized rotation matrix functions

--- common/camera.c
+++ common/camera.c
@@ -1,39 +1,75 @@
 #include "camera.h"
 
 struct Camera* camera_create(double pos[], double rot[], int res[],
-                             double fov, double focal_length) {
+                             double fov, double focal_length, double light[]) {
+    bool success = true;
     struct Camera* C = malloc(sizeof(struct Camera));
+    camera_set_slow(C, false);
+    camera_set_position(C, pos, rot);
+    camera_set_light(C, light);
+    if (!camera_set_resolution(C, res)) success = false;
+    if (!camera_set_focal_length(C, focal_length)) success = false;
+    if (!camera_set_fov(C, fov)) success = false;
+    if (!camera_set_mode(C, VERTEX_RENDER)) success = false;
+    camera_calc_array_size(C);
+    camera_update_rotation(C);
+
+    if (success) return C;
+    else return NULL;
+}
+
+void camera_set_position(struct Camera* C, double pos[], double rot[]) {
     for (int i = 0; i < 3; i++) {
         C->pos[i] = pos[i];
         C->rot[i] = fmod(rot[i], M_PI*2);
         C->acc[i] = 0;
         C->vel[i] = 0;
     }
+}
+
+void camera_set_light(struct Camera* C, double light[]) {
+    for (int i = 0; i < 3; i++) {
+        C->light[i] = light[i];
+    }
+}
+
+bool camera_set_resolution(struct Camera* C, int res[]) {
     for (int i = 0; i < 2; i++) {
         if (res[i] > 0) C->res[i] = res[i];
         else {
-            fprintf(stderr, "previewer: invalid resolution -- %dx%dpx\n",
+            fprintf(stderr, "previewer: invalid resolution -- %dx%d px\n",
                     res[0], res[1]);
-            return NULL;
+            return false;
         }
     }
+    return true;
+}
 
+bool camera_set_focal_length(struct Camera* C, double focal_length) {
     if (focal_length > 0) C->focal_length = focal_length;
     else {
-        fprintf(stderr, "previewer: invalid focal length -- %f m\n", focal_length);
-        return NULL;
+        fprintf(stderr, "camera: invalid focal length -- %f m\n", focal_length);
+        return false;
     }
+    return true;
+}
+
+bool camera_set_fov(struct Camera* C, double fov) {
     if (fov > 0 && fov < M_PI) C->fov = fov;
     else {
-        fprintf(stderr, "previewer: invalid field of view");
-        return NULL;
+        fprintf(stderr, "camera: invalid field of view -- %f radians\n", fov);
+        return false;
     }
+    return true;
+}
 
-    camera_calc_array_size(C);
-    C->pixel_size = C->array_width / res[0];
-    camera_update_rotation(C);
-
-    return C;
+bool camera_set_mode(struct Camera* C, int mode) {
+    if (mode >= 0 && mode < 2) C->mode = mode;
+    else {
+        fprintf(stderr, "camera: invalid mode -- %d\n", mode);
+        return false;
+    }
+    return true;
 }
 
 void camera_calc_array_size(struct Camera* C) {
@@ -64,37 +100,18 @@ void camera_calc_array_size(struct Camera* C) {
 
     C->array_width = tan(C->fov/2) * 2 * C->focal_length;
     C->array_height = C->array_width * C->res[1] / C->res[0];
+    C->pixel_size = C->array_width / C->res[0];
 }
 
 void camera_calc_rotation_matrix(struct Camera* C) {
     double rot_x[3][3], rot_y[3][3], rot_z[3][3];
 
-    /* rotation matrix for x axis (yaw):
-     *         | 1   0      0       |
-     * Rx(a) = | 0   cos(a) -sin(a) |
-     *         | 0   sin(a) cos(a)  | */
-    rot_x[0][0] = 1; rot_x[0][1] = 0;               rot_x[0][2] = 0;
-    rot_x[1][0] = 0; rot_x[1][1] = C->frame_cos[0]; rot_x[1][2] = -C->frame_sin[0];
-    rot_x[2][0] = 0; rot_x[2][1] = C->frame_sin[0]; rot_x[2][2] = C->frame_cos[0];
-
-    /* rotation matrix for y axis (pitch)
-     *         | cos(b)  0   sin(b) |
-     * Ry(b) = | 0       1   0      |
-     *         | -sin(b) 0   cos(b) | */
-    rot_y[0][0] = C->frame_cos[1];  rot_y[0][1] = 0; rot_y[0][2] = C->frame_sin[1];
-    rot_y[1][0] = 0;                rot_y[1][1] = 1; rot_y[1][2] = 0;
-    rot_y[2][0] = -C->frame_sin[1]; rot_y[2][1] = 0; rot_y[2][2] = C->frame_cos[1];
-
-    /* rotation matrix for z axis (roll)
-     *         | cos(c) -sin(c) 0 |
-     * Rz(c) = | sin(c) cos(c)  0 |
-     *         | 0      0       1 | */
-    rot_z[0][0] = C->frame_cos[2]; rot_z[0][1] = -C->frame_sin[2]; rot_z[0][2] = 0;
-    rot_z[1][0] = sin(C->rot[2]);  rot_z[1][1] = C->frame_cos[2];  rot_z[1][2] = 0;
-    rot_z[2][0] = 0;               rot_z[2][1] = 0;                rot_z[2][2] = 1;
+    matrix_rotation_x(C->frame_cos[0], C->frame_sin[0], rot_x);
+    matrix_rotation_y(C->frame_cos[1], C->frame_sin[1], rot_y);
+    matrix_rotation_z(C->frame_cos[2], C->frame_sin[2], rot_z);
 
     /* combined rotation matrix
-     * R = Rx(a) * Ry(b) * Rz(c) */
+     * R = Rz(c) * Ry(b) * Rx(a) */
     double rot_z_rot_y[3][3];
     matrix_product(3, 3, rot_z,
                    3, 3, rot_y,
@@ -102,6 +119,17 @@ void camera_calc_rotation_matrix(struct Camera* C) {
     matrix_product(3, 3, rot_z_rot_y,
                    3, 3, rot_x,
                    C->rotation_matrix);
+
+    matrix_rotation_x(C->frame_cos_rev[0], C->frame_sin_rev[0], rot_x);
+    matrix_rotation_y(C->frame_cos_rev[1], C->frame_sin_rev[1], rot_y);
+    matrix_rotation_z(C->frame_cos_rev[2], C->frame_sin_rev[2], rot_z);
+
+    matrix_product(3, 3, rot_z,
+                   3, 3, rot_y,
+                   rot_z_rot_y);
+    matrix_product(3, 3, rot_z_rot_y,
+                   3, 3, rot_x,
+                   C->rotation_matrix_rev);
 }
 
 void camera_calc_pixel_direction(struct Camera C, int row, int col, double V[]) {
@@ -110,7 +138,7 @@ void camera_calc_pixel_direction(struct Camera C, int row, int col, double V[]) 
      *  |---------------|  c ^         ^ y        [m]
      *                       |  _______|_______         _
      *  ^ y             |    H |_|_|_|_|_|_|_|_|        |
-     *  |   z           |    : |_|_|_|_|_|_|_|_|   x    |
+     *  |  -z           |    : |_|_|_|_|_|_|_|_|   x    |
      *  C -->  -  -  -  |    2 |_|_|_|_C_|_|_|_|--->  height
      *                  |    1 |_|_|_|_|_|_|_|_|        |
      *                  |    0 |_|_|_|_|_|_|_|_|        |
@@ -120,23 +148,16 @@ void camera_calc_pixel_direction(struct Camera C, int row, int col, double V[]) 
      * W, H: resolution width, height
      * r, c: row, column
      *
-     * First, create a vertex for a point in an unrotated array
-     * with origo located inside the camera. The vertex will be
-     * equal to the vector representing the direction of the ray
-     * once rotated with camera rotation.
      * V0 = (x, y, z)
      * x = -width  / 2 + (col+0.5) * pixel_size
      * y = -height / 2 + (row+0.5) * pixel_size
-     * z = focal_length
-     *
-     * then add the camera's rotation to the vector by multiplying the vector
-     * with the camera's rotation matrix:
+     * z = -focal_length
      *
      * V = (x', y', z')
      *
-     *       |x'|              |x|
-     * V^T = |y'| = R*V0^T = R*|y|
-     *       |z'|              |z| */
+     *     |x'|            |x|
+     * V = |y'| = R*V0 = R*|y|
+     *     |z'|            |z| */
 
     double unrotated_V[3][1], rotated_V[3][1];
 
@@ -150,6 +171,11 @@ void camera_calc_pixel_direction(struct Camera C, int row, int col, double V[]) 
 
     /* copy coordinates from column matrix to row vector V */
     for (int i = 0; i < 3; i++) V[i] = rotated_V[i][0];
+}
+
+bool camera_faces_point(struct Camera* C, double point[]) {
+    double sign = dot_product(C->direction, point);
+    return sign + C->signed_distance > 0;
 }
 
 void camera_set_slow(struct Camera* C, bool s) { C->slow = s; }
@@ -166,11 +192,22 @@ void camera_change_fov(struct Camera* C, bool increase) {
     camera_calc_array_size(C);
 }
 
+void camera_stop(struct Camera* C) {
+    for (int i = 0; i < 3; i++) {
+        C->vel[i] = 0;
+        C->acc[i] = 0;
+    }
+}
+
 void camera_move_forward(struct Camera* C) { 
-    camera_move_horizontal(C, -CAMERA_SPEED, C->rot[1]);
+    double acceleration[3];
+    vector_multiply(CAMERA_SPEED, C->direction, acceleration);
+    vector_add(C->vel, acceleration, C->vel);
 }
 void camera_move_back(struct Camera* C) {
-    camera_move_horizontal(C, CAMERA_SPEED, C->rot[1]);
+    double acceleration[3];
+    vector_multiply(CAMERA_SPEED, C->direction, acceleration);
+    vector_subtract(C->vel, acceleration, C->vel);
 }
 void camera_move_right(struct Camera* C) { 
     camera_move_horizontal(C, -CAMERA_SPEED, C->rot[1] + M_PI / 2);
@@ -195,9 +232,9 @@ void camera_move_vertical(struct Camera* C, double d ) {
 }
 void camera_accelerate(struct Camera* C, int i, double a) { C->vel[i] += a; }
 
-void camera_rotate(struct Camera* C, int u, int v) {
-    C->rot[0] = fmod(C->rot[0] + v*MOUSE_SENSITIVITY, M_PI*2);
-    C->rot[1] = fmod(C->rot[1] + u*MOUSE_SENSITIVITY, M_PI*2);
+void camera_rotate(struct Camera* C, int x, int y) {
+    C->rot[0] = fmod(C->rot[0] + y*MOUSE_SENSITIVITY, M_PI*2);
+    C->rot[1] = fmod(C->rot[1] + x*MOUSE_SENSITIVITY, M_PI*2);
 }
 
 void camera_frame_update(struct Camera* C, int fps) {
@@ -209,6 +246,8 @@ void camera_update_rotation(struct Camera* C) {
     for (int i = 0; i < 3; i++) {
         C->frame_cos[i] = cos(C->rot[i]);
         C->frame_sin[i] = sin(C->rot[i]);
+        C->frame_cos_rev[i] = C->frame_cos[i]; /* cos(-a) = cos(a) */
+        C->frame_sin_rev[i] = -C->frame_sin[i]; /* sin(-a) = -sin(a) */
     }
     camera_calc_rotation_matrix(C);
     static double camera_dir_unrotated[3][1] = {{0},{0}, {1}};
@@ -217,6 +256,7 @@ void camera_update_rotation(struct Camera* C) {
                    3, 1, camera_dir_unrotated,
                    camera_dir_rotated);
     for (int i = 0; i < 3; i++) C->direction[i] = camera_dir_rotated[i][0];
+    C->signed_distance = -dot_product(C->direction, C->pos);
 }
 
 void camera_update_position(struct Camera* C, int fps) {
@@ -228,3 +268,7 @@ void camera_update_position(struct Camera* C, int fps) {
     }
 }
 
+int camera_toggle_mode(struct Camera* C) {
+    C->mode = !C->mode;
+    return C->mode;
+}
--- common/camera.h
+++ common/camera.h
@@ -9,6 +9,9 @@
 #include "../common/vector.h"
 #include "../common/matrix.h"
 
+#define VERTEX_RENDER 0
+#define SURFACE_RENDER 1
+
 #define CAMERA_SPEED 1
 #define CAMERA_VELOCITY_LOSS 0.9
 #define CAMERA_SLOW_MULTIPLIER 0.1
@@ -24,24 +27,38 @@ struct Camera {
     double focal_length;
     double fov;
     int res[2];
-    double direction[3];
+    double direction[3]; /* plane normal */
+    double signed_distance;
+    double light[3];
     double pixel_size;
     double rotation_matrix[3][3];
+    double rotation_matrix_rev[3][3];
 
+    int mode;
     bool slow;
     double frame_cos[3];
     double frame_sin[3];
+    double frame_cos_rev[3];
+    double frame_sin_rev[3];
 };
 
 struct Camera* camera_create(double pos[], double rot[], int res[], 
-                             double fov, double focal_lengt);
+                             double fov, double focal_length, double light[]);
+void camera_set_position(struct Camera* C, double pos[], double rot[]);
+void camera_set_light(struct Camera* C, double light[]);
+bool camera_set_resolution(struct Camera* C, int res[]);
+bool camera_set_focal_length(struct Camera* C, double focal_length);
+bool camera_set_fov(struct Camera* C, double fov);
+bool camera_set_mode(struct Camera* C, int mode);
 void camera_calc_array_size(struct Camera* C);
 void camera_calc_rotation_matrix(struct Camera* C);
 void camera_calc_pixel_direction(struct Camera C, int row, int col, double V[]);
+bool camera_faces_point(struct Camera* C, double point[]);
 void camera_set_slow(struct Camera* C, bool s);
 void camera_increase_fov(struct Camera* C);
 void camera_decrease_fov(struct Camera* C);
 void camera_change_fov(struct Camera* C, bool increase);
+void camera_stop(struct Camera* C);
 void camera_move_forward(struct Camera* C);
 void camera_move_back(struct Camera* C);
 void camera_move_right(struct Camera* C);
@@ -55,5 +72,6 @@ void camera_rotate(struct Camera* C, int u, int v);
 void camera_frame_update(struct Camera* C, int fps);
 void camera_update_rotation(struct Camera* C);
 void camera_update_position(struct Camera* C, int fps);
+int camera_toggle_mode(struct Camera* C);
 
 #endif
